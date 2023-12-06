0cf1aeda90c5ab3c09e64ad5b80241e4299108d8
Ajay <pajay7686@gmail.com>
2023-12-06 18:11:21 +0530

    cards done

--- src/components/hooks/useGetMovieVideo.js
+++ src/components/hooks/useGetMovieVideo.js
@@ -17,7 +17,8 @@ const useGetMovieVideo = (movieid) => {
   };
   useEffect(() => {
     getMovieVideo();
-    // eslint-disable-next-line
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 };
 
