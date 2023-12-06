e340d0f573e467661a2881edf658a1662cd4e63c
Ajay <pajay7686@gmail.com>
2023-12-06 19:50:07 +0530

    Scrollbar Hide Fix

--- src/components/Movielist.js
+++ src/components/Movielist.js
@@ -4,9 +4,9 @@ import Moviecard from "./Moviecard";
 const Movielist = ({ title, movies }) => {
  
   return (
-    <div className="p-4 ">
+    <div className="p-2 ml-6 ">
       <h1 className="text-2xl pl-2 text-white">{title}</h1>
-      <div className="flex overflow-x-scroll  ">
+      <div className="flex overflow-x-scroll no-scrollbar ">
         <div className="flex ">
           {movies?.map((movie) => (
             <Moviecard key={movie?.id} postpath={movie?.poster_path} />
